98ef93e2dbad475369e2e1b5eb5296f51b65f1d0
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-03-15 11:05:24 +0100

    add implementation of class Man

--- src/Man.java
+++ src/Man.java
@@ -3,4 +3,9 @@ public class Man extends Human{
   public Man(String name) {
     super(name);
   }
+
+  @Override
+  public String toString() {
+    return String.format("Мужчина по имени %s", super.getName());
+  }
 }
