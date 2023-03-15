72817a8f7158f3a07a73a6160270ffd339343d0f
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-03-15 11:00:48 +0100

    add implementation of class Human

--- src/Human.java
+++ src/Human.java
@@ -1,2 +1,21 @@
 public class Human {
+  private final String name;
+
+  public Human(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Человек по имени %s", name);
+  }
+
+  public void sayHi() {
+    System.out.println("Я родился!");
+  }
+
 }
