6b0171244e7a311fb7d5fcd635939534a7dc8927
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-03-15 11:10:03 +0100

    add test code in class Main

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,12 @@ public class Main {
     h1.sayHi();
     System.out.println(h1);
 
+    Man m1 = new Man("Mike");
+    m1.sayHi();
+    System.out.println(m1);
+
+    Woman w1 = new Woman("Margo");
+    w1.sayHi();
+    System.out.println(w1);
   }
 }
