70f68d1e97db214ee571542e1320d6baf884b2d7
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-03-15 11:09:10 +0100

    add implementation of class Woman

--- src/Woman.java
+++ src/Woman.java
@@ -1,2 +1,16 @@
-public class Woman extends Human{
+public class Woman extends Human {
+  public Woman(String name) {
+    super(name);
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Женщина по имени %s", super.getName());
+  }
+
+  @Override
+  public void sayHi() {
+    System.out.println("Я родилась!");
+  }
+
 }
